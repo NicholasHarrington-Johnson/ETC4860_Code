5a1b7db8c13cc604e2a759ec0de56bcc4be79a2b
NicholasHarrington-Johnson <nicholas.harringtonjohnson@gmail.com>
2015-09-09 11:32:28 +1000

    This code is the starting blocks for running on the cluster

--- cluster_main.R
+++ cluster_main.R
@@ -1,13 +1,11 @@
 ## Call all file
 rm(list=ls())
 
+# Run this on the cluster for everything to work nice
+
 ## Loading packages
 
 source("packages.R")
-
-## Reading data
-
-eztable <- fread("booking_top30_restaurants_all.txt")
 
 ## Loading functions
 
@@ -19,41 +17,10 @@ phols <- readph(phols)
 
 ## Bookings
 numr <- c(1:30)[-17]
-#r <- clean(eztable)
-
-## Truncating data
-
-#tr <- truncatep(r)
-#save(tr,file="truncated_data.Rda")
 
 load("truncated_data.Rda")
-## Plotting
-
-for(i in numr)
-{
-  plotpub(tr[[i]],i)
-}
-
-## Best overall model
-
-#out <- bmod(tr)
-#print(out)
-
-#rstnum <-2
-#plotpub(tr[[rstnum]],rstnum)
-#out <- mseevaluate(tr[[rstnum]],h=14)
-
-#print(out[[1]])
-#plotmse(out[[2]])
-#ploth(out[[3]])
 
 # Run for all restaurants and output to an R file
-numr <- c(1:3)[-2]
-all_mses <- list()
-system.time(
-for (rstnum in numr){
-  all_mses[[rstnum]] <- rest_mod(tr[[rstnum]],starttraining=(nrow(tr[[rstnum]])-3),h=2)
-})
 
 cl <- makeCluster(8)
 registerDoParallel(cl)
@@ -69,6 +36,8 @@ all_mses <- list()
 pckz <- c("data.table","dplyr","fpp","timeDate","splines","doParallel")
 
 all_mses <- foreach(rstnum = numr,.combine='comb',.multicombine = TRUE,.packages = pckz) %dopar%
-     rest_mod(tr[[rstnum]],starttraining=(nrow(tr[[rstnum]])-3),h=2)
+     rest_mod(tr[[rstnum]],starttraining=50,h=14)
 
 save(all_mses,file="all_mses.Rda")
+
+stopCluster(cl)
